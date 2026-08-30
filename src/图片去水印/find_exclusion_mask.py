b6544411bfd275fbb86b6450311824576d3acbb2
Git_Winfrise <>
2026-08-30 15:44:22 +0800

    完成

--- src/图片去水印/find_exclusion_mask.py
+++ src/图片去水印/find_exclusion_mask.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 
-def find_exclusion_mask(image_path, debug_mode = False):
+def find_exclusion_mask(image_path, watermark_path, debug_mode = False):
     """
     检测图片中的目标区域。
     :param image_path: 图片路径
@@ -54,42 +54,44 @@ def find_exclusion_mask(image_path, debug_mode = False):
                 # 2. 再过二次纹理密度筛选
                 # 注意：这里x设为0，w设为全宽
                 box = (0, raw_start_y, w, raw_height)
-                
 
-                print(f"✅ 发现有效区域: Y[{raw_start_y}-{raw_end_y}], 高度: {raw_height}")
                 detected_boxes.append(box)
+
+                if debug_mode:
+                    print(f"✅ 发现有效区域: Y[{raw_start_y}-{raw_end_y}], 高度: {raw_height}")
             else:
-                print(f"❌ 高度不符 (要求40-150px): 实际 {raw_height}px")
+                if debug_mode:
+                    print(f"❌ 高度不符 (要求40-150px): 实际 {raw_height}px")
 
     # 循环结束后，如果还在区域内，需要处理最后一段（防止图片底部截断）
     if in_region:
-         end_y = len(is_pattern_row)
-         raw_start_y = int(start_y * scale_ratio)
-         raw_end_y = int(end_y * scale_ratio)
-         raw_height = raw_end_y - raw_start_y
-         if 40 < raw_height < 150:
+        end_y = len(is_pattern_row)
+        raw_start_y = int(start_y * scale_ratio)
+        raw_end_y = int(end_y * scale_ratio)
+        raw_height = raw_end_y - raw_start_y
+        if 40 < raw_height < 150:
             box = (0, raw_start_y, w, raw_height)
             detected_boxes.append(box)
 
 
 
     if debug_mode:
-        print(f"\n🎉 检测完成！共找到 {len(detected_boxes)} 个区域。")
-        
-        # 2. 在图片上画框 (保存图片的功能放在这里)
-        for box in detected_boxes:
-            x, y, w, h = box
-            # 画红色矩形框，线宽2
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        if len(detected_boxes) > 0:
+            print(f"\n🎉 检测完成！共找到 {len(detected_boxes)} 个区域。")
             
-        # 3. 保存图片
-        output_dir = os.path.dirname(image_path)
-        output_name = os.path.join(output_dir, "result_detected.jpg")
-        cv2.imwrite(output_name, img)
-        print(f"💾 结果图片已保存至: {output_name}")
-        
-    else:
-        print("\n😭 未找到任何匹配区域，图片未保存。")
+            # 2. 在图片上画框 (保存图片的功能放在这里)
+            for box in detected_boxes:
+                x, y, w, h = box
+                # 画红色矩形框，线宽2
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                
+            # 3. 保存图片
+            output_dir = os.path.dirname(image_path)
+            output_name = os.path.join(output_dir, "result_detected.jpg")
+            cv2.imwrite(output_name, img)
+            print(f"💾 结果图片已保存至: {output_name}")
+        else:
+            print("\n😭 未找到任何匹配区域，图片未保存。")
 
 
     return detected_boxes

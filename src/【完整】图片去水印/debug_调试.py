d62e05e3c67fa66a21719c524d5c56d2e165a91e
Git_Winfrise <>
2026-08-30 20:03:27 +0800

    调试代码

--- src/【完整】图片去水印/debug_调试.py
+++ src/【完整】图片去水印/debug_调试.py
@@ -1,18 +1,33 @@
 from find_exclusion_mask import find_exclusion_mask
+from remove_watermark import remove_watermark
 
 if __name__ == "__main__":
-    # 1. 排除区域代码调试
-    image_path = "/Users/teacher/Desktop/test/single/page316_img1.png"
-    debug_mode = True
 
-    results = find_exclusion_mask(
-        image_path = image_path,
-        debug_mode = debug_mode,
-    )
-    
-    if results:
-        print(f"\n🎉 检测完成！共找到 {len(results)} 个区域。")
-        for i, box in enumerate(results):
-            print(f"区域 {i+1}: x={box[0]}, y={box[1]}, w={box[2]}, h={box[3]}")
+    debug_full = True
+    if debug_full:
+        image_path = "/Users/teacher/Desktop/test/page323_img1.png"
+        watermark_path = "/Users/teacher/Desktop/test/mask.png"
+        debug_mode = True
+
+        remove_watermark(
+            image_path=image_path, 
+            watermark_path=watermark_path, 
+            exclusion_func=find_exclusion_mask,
+            debug_mode = debug_mode
+        )
     else:
-        print("\n😭 未找到任何匹配区域。")
+        # 1. 排除区域代码调试
+        image_path = "/Users/teacher/Desktop/test/single/page316_img1.png"
+        debug_mode = True
+
+        results = find_exclusion_mask(
+            image_path = image_path,
+            debug_mode = debug_mode,
+        )
+        
+        if results:
+            print(f"\n🎉 检测完成！共找到 {len(results)} 个区域。")
+            for i, box in enumerate(results):
+                print(f"区域 {i+1}: x={box[0]}, y={box[1]}, w={box[2]}, h={box[3]}")
+        else:
+            print("\n😭 未找到任何匹配区域。")
--- src/【完整】图片去水印/main_图片去水印.py
+++ src/【完整】图片去水印/main_图片去水印.py
@@ -19,14 +19,12 @@ from utils import batch_process_file_with_callback
 if __name__ == "__main__":
     image_path = "/Users/teacher/Desktop/test/page323_img1.png"
     watermark_path = "/Users/teacher/Desktop/test/mask.png"
-    debug_mode = True
 
     if os.path.isfile(image_path):
         remove_watermark(
             image_path=image_path, 
             watermark_path=watermark_path, 
             exclusion_func=find_exclusion_mask,
-            debug_mode = debug_mode
         )
     elif os.path.isdir(image_path):
         def callback_func (input_file, output_file):

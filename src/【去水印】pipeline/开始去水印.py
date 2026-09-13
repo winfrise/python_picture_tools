34ca77bb580787359c3d56daeb537935ba8fd13f
Git_Winfrise <>
2026-09-13 16:04:29 +0800

    添加批量功能

--- src/【去水印】pipeline/开始去水印.py
+++ src/【去水印】pipeline/开始去水印.py
@@ -3,10 +3,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from run_watermark_pipeline import run_watermark_pipeline
 from helpers.color_to_gray import color_to_gray
 from helpers.hex_to_rgb import hex_to_rgb
+from utils import batch_process_file_with_callback
 
 # ================= 3. 测试运行 =================
 if __name__ == "__main__":
-    image_path = "/Users/teacher/Desktop/青岛大学附属医院总务设备采购项目002(二次)-证书-测试/证书/test11/青岛大学_页面_009.jpg"
+    image_path = "/Users/teacher/Desktop/xxx/test11"
     pipeline_steps = [
         {
             "watermark_area_img": None,
@@ -30,5 +31,28 @@ if __name__ == "__main__":
     ]
 
 
-    run_watermark_pipeline(image_path, pipeline_steps)
+
+    if os.path.isfile(image_path):
+        base_name, ext = os.path.splitext(image_path)
+        output_path = f"{base_name}_output_智能填充{ext}"
+
+        run_watermark_pipeline(image_path, pipeline_steps, output_path)
+    elif os.path.isdir(image_path):
+        input_dir = image_path
+        def callback_func(input_file, output_file):
+            run_watermark_pipeline(
+                image_path=input_file,
+                output_path=output_file,
+                steps = pipeline_steps
+            )
+
+        output_dir = f"{input_dir}_output_智能去水印"
+        batch_process_file_with_callback(
+            input_dir=input_dir,
+            output_dir=output_dir,
+            callback_func=callback_func,
+        )
+    else:
+        print(f"路径错误: {image_path}")
+
 

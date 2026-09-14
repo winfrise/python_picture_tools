6257e154a1b0983b1add6b8b2923a9ff0b915377
Git_Winfrise <>
2026-09-14 11:19:30 +0800

    优化代码

--- src/【去水印】pipeline/tools/fill_watermark_with_color.py
+++ src/【去水印】pipeline/tools/fill_watermark_with_color.py
@@ -8,18 +8,18 @@ FILL_COLOR = [0, 0, 0]
 def fill_watermark_with_color(img, mask, fill_color = [255, 255, 255]):
     if img is None or mask is None:
         raise ValueError("输入参数 img 和 mask 均不能为 None")
-
+    
 
     if fill_color is None or fill_color == '':
         print("⚠️ 警告：未检测到填充颜色")
         raise
 
     img = img.copy()
-    
+    fill_color_gbr = fill_color[::-1] 
     # 获取所有需要填充的水印像素坐标
     ys, xs = np.where(mask > 0)
     
     for y, x in zip(ys, xs):
-        img[y, x] = fill_color
+        img[y, x] = fill_color_gbr
     
     return img
--- src/【去水印】pipeline/开始去水印.py
+++ src/【去水印】pipeline/开始去水印.py
@@ -7,22 +7,23 @@ from utils import batch_process_file_with_callback
 
 # ================= 3. 测试运行 =================
 if __name__ == "__main__":
-    image_path = "/Users/teacher/Desktop/xxx/test11"
+    image_path = "/Users/teacher/Desktop/青岛投标去水印/证书-测试/证书/02"
     pipeline_steps = [
         {
             "watermark_area_img": None,
-            "target_gray": color_to_gray("#bcbeb9"),
+            # "watermark_area_img": "/Users/teacher/Desktop/青岛投标去水印/证书-测试/证书/02_mask.png",
+            "target_gray": color_to_gray("#d2d4d3"),
             "target_gray_threshold": 30,
             # "fill_color": [255, 255, 255]
-            "fill_color": hex_to_rgb("#d4ebdb")
+            "fill_color": hex_to_rgb("#daecf8")
         },
         {
             "watermark_area_img": None,
-            "target_gray": color_to_gray("#757774"),
+            "target_gray": color_to_gray("#827473"),
             "target_gray_threshold": 20,
             "exclude_rgb_list": [
                 {
-                    "color_rgb": hex_to_rgb("#4f9d73"),
+                    "color_rgb": hex_to_rgb("#000000"),
                     "color_threshold": 50,
                 }
             ],
